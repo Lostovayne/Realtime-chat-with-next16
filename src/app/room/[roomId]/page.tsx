69dc22deb06096cba1dabb7daafd481b55101088
Deus lo Vult <92962731+Lostovayne@users.noreply.github.com>
2025-12-12 19:45:37 -0300

    feat: Update dependencies and enhance room page with input handling and destroy button

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function formatTimeRemaining(seconds: number) {
   const minutes = Math.floor(seconds / 60);
@@ -15,6 +15,8 @@ export default function Room() {
 
   const [copyStatus, setCopyStatus] = useState<"COPY" | "COPIED">("COPY");
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+  const [input, setInput] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const copyLink = () => {
     const url = `${window.location.origin}/room/${roomId}`;
@@ -58,7 +60,39 @@ export default function Room() {
             </span>
           </div>
         </div>
+
+        <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white transition-all font-bold group flex items-center gap-2 disabled:opacity-50 uppercase cursor-pointer disabled:cursor-not-allowed">
+          <span className="group-hover:animate-pulse">♨️</span>
+          Destroy Now
+        </button>
       </header>
+
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4 ">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+              {">"}
+            </span>
+            <input
+              autoFocus
+              type="text"
+              placeholder="Type message..."
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && input.trim()) {
+                  inputRef.current?.focus();
+                }
+              }}
+            />
+          </div>
+          <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 uppercase cursor-pointer disabled:cursor-not-allowed">
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
